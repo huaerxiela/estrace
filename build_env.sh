d3a5450c336fff4f51d2683f10166a6d252cfadd
hexl <hexl@hexl.com>
2025-08-05 18:45:54 +0800

    update

--- build_env.sh
+++ build_env.sh
@@ -1,7 +1,10 @@
 cd ..
-git clone --branch b50db1997e47bab29d9278f4b4ee1dabe981b8c4 https://github.com/SeeFlowerX/ebpf
-git clone --branch e54e4137aea3782bd97834e1dc0d729182b630a6 https://github.com/SeeFlowerX/ebpfmanager
-cd -
+git clone https://github.com/SeeFlowerX/ebpf
+cd ebpf && git reset --hard b50db1997e47bab29d9278f4b4ee1dabe981b8c4 && cd ..
+git clone https://github.com/SeeFlowerX/ebpfmanager
+cd ebpfmanager && git reset --hard e54e4137aea3782bd97834e1dc0d729182b630a6 && cd ..
+
+cd estrace
 mkdir -p external && cd external
 git clone https://android.googlesource.com/platform/bionic --depth=1
 mkdir system && cd system
